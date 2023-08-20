32f9bfdef5bc7b824ab69503422c4d04e6d0a701
Kevin Silva <kevincaprino33@gmail.com>
2023-08-20 14:55:31 -0300

    Rota de funcionarios

--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResource('/teste', EmployeeController::class);
+Route::apiResource('/employee', EmployeeController::class);
