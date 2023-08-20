c1a7d87d863941faaa7395548d9aa5b2c17c6a1b
Kevin Silva <kevincaprino33@gmail.com>
2023-08-20 18:18:44 -0300

    Model de contratantes

---
+++ app/Models/Contractor.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+
+class Contractor extends Authenticatable implements HasMedia
+{
+    use HasApiTokens, HasFactory, Notifiable, InteractsWithMedia;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'document',
+        'phone',
+        'state',
+        'city',
+        'neighborhood',
+        'street',
+        'number',
+        'zipcode',
+        'complement',
+        'archived',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var array<int, string>
+     */
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+
+    public function registerMediaCollections(): void
+    {
+        $this->addMediaCollection('thumb')->singleFile();
+    }
+}
