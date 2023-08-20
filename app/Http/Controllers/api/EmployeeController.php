43215857782812f8431aa1c6ce9be9c7d599b4d6
Kevin Silva <kevincaprino33@gmail.com>
2023-08-20 17:24:15 -0300

    Controller de funcionarios

---
+++ app/Http/Controllers/api/EmployeeController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Repositories\EmployeeRepository;
+use Illuminate\Http\Request;
+
+class EmployeeController extends Controller
+{
+    private $employeeRepository;
+
+    function __construct(EmployeeRepository $employeeRepository)
+    {
+        $this->employeeRepository = $employeeRepository;
+    }
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        return $this->employeeRepository->getEmployee()->get()->values();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $employee = $this->employeeRepository->store($request->all());
+        return $employee;
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return $this->employeeRepository->getEmployee()->find($id);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, int $id)
+    {
+        return $this->employeeRepository->update($request->all(), $id);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        return $this->employeeRepository->getEmployee()->find($id)->delete();
+    }
+}
